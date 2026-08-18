08c2647144ab329ecf7c2992a5233d2bef8e6698
Tanmoy Biswas <tanmoybiswas478@gmail.com>
2026-08-18 18:36:07 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,12 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\File;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
-
-// Tumhare baaki ke web routes yahan honge (agar koi hain toh)
-
-// Sabse last mein yeh catch-all route daalo
 Route::get('/{any?}', function () {
-    return File::get(public_path('index.html'));
+    $path = public_path('index.html');
+    
+    if (File::exists($path)) {
+        return File::get($path);
+    }
+    
+    return view('welcome');
 })->where('any', '.*');
