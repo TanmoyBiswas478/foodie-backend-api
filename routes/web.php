a3fcf7503df6f71e15871d2978eee19a4528e506
tanmoybiswas478 <sranit55@gmail.com>
2026-08-19 11:34:54 +0530

    Fix direct file response routing for Angular build

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,17 @@
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    $path = public_path('home/index.html');
+    if (file_exists($path)) {
+        return response()->file($path);
+    }
+    return 'Home index.html not found at: ' . $path;
+});
+
+Route::get('/{any}', function ($any) {
+    $path = public_path($any . '/index.html');
+    if (file_exists($path)) {
+        return response()->file($path);
+    }
+    abort(404);
+})->where('any', '.*');
