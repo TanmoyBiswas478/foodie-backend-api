853f45c3efc0f495bc544f3877a0c23f7c6921f9
tanmoybiswas478 <sranit55@gmail.com>
2026-08-19 11:37:00 +0530

    Add debug path info for 404

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,21 @@
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $path = public_path('home/index.html');
-    if (file_exists($path)) {
-        return response()->file($path);
-    }
-    return 'Home index.html not found at: ' . $path;
-});
+Route::get('/{any?}', function ($any = null) {
+    // Agar koi path nahi diya toh 'home' man lo
+    $folder = empty($any) ? 'home' : trim($any, '/');
+    
+    // Alag-alag paths check karte hain
+    $path1 = public_path($folder . '/index.html');
+    $path2 = public_path('browser/' . $folder . '/index.html'); // Agar angular 'browser' folder mein build hua ho
 
-Route::get('/{any}', function ($any) {
-    $path = public_path($any . '/index.html');
-    if (file_exists($path)) {
-        return response()->file($path);
+    if (file_exists($path1)) {
+        return response()->file($path1);
     }
-    abort(404);
+    
+    if (file_exists($path2)) {
+        return response()->file($path2);
+    }
+
+    // Agar kahin nahi mili, toh screen par paths dikha do taaki error pakad mein aaye
+    return response("Debug Info: <br> Tried path 1: " . $path1 . "<br> Tried path 2: " . $path2 . "<br> Public path root: " . public_path(), 404);
 })->where('any', '.*');
